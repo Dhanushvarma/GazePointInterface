9a29438e949a8189663e487734e3601f9aa71624
Dhanushvarma <pdhanu2001@gmail.com>
2024-11-19 12:09:52 -0800

    fix : added configs to part of the __init__.py

--- gazepointinterface/__init__.py
+++ gazepointinterface/__init__.py
@@ -1,5 +1,5 @@
-from gazepointinterface.gaze_sensor_server import GazepointClient, DataForwardingServer
-from gazepointinterface.sim_client.gaze_data_client import SimGazeClient
+from gazepointinterface.gaze_sensor_server import GazepointClient, DataForwardingServer, GazepointConfig
+from gazepointinterface.sim_client.gaze_data_client import SimGazeClient, GazeServerConfig
 from gazepointinterface.sim_client.gaze_data_processor import GazeDataUtil
 
 __version__ = "0.1.0"
--- gazepointinterface/sim_client/__init__.py
+++ gazepointinterface/sim_client/__init__.py
@@ -1,2 +1,2 @@
-from .gaze_data_client import SimGazeClient
+from .gaze_data_client import SimGazeClient, GazeServerConfig
 from .gaze_data_processor import GazeDataUtil
